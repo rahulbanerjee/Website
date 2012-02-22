7b123f78ccf8ae2654b9992c4d0a96d6c8119657
Rahul Banerjee <banerjee@cs.washington.edu>
2012-02-22 02:20:48 -0800

    need to copy .htaccess back over

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,6 @@ ROOT="$HOME/Website"
 rm -rf "$ROOT/publish"
 mkdir -p "$ROOT/publish"
 ./scripts/python/generate.py $ROOT/templates/site.html $ROOT/content $ROOT/publish
+cp $ROOT/templates/.htaccess $ROOT/publish
 cd $ROOT/publish
 ln -s overview.html index.html
